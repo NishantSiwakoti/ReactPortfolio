d6ef9b3d64b9c3a678896cf3c5f64ac617e3af42
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-04-10 12:04:23 +0545

    hero section completed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
   }, [darkMode]);
 
   const activeClass =
-    "block py-2 px-3 text-white bg-orange-400 rounded md:bg-transparent md:text-orange-400 md:p-0 dark:text-white md:dark:text-blue-500";
+    "block py-2 px-3 text-white bg-orange-400 rounded md:bg-transparent md:text-orange-400 md:p-0 dark:text-white md:dark:text-orange-500";
   const inActiveClass =
     "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
   return (
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,51 @@
-import photo from "../assets/images/nishant.png";
+import { TypeAnimation } from "react-type-animation";
+import photo from "../assets/images/animation.png";
+
 const Home = () => {
   return (
-    <section className="flex flex-col md:flex-row items-center">
-      <div className="my-5">
-        <h1 className="text-5xl font-bold">Hi, I'm Nishant</h1>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          Pink to Orange
-        </button>
+    <section className="md:flex items-center">
+      <div className="flex-1 pl-5">
+        <h1 className="text-orange-400 text-3xl text-center md:text-left md:ml-[100px] ">
+          Hi,
+        </h1>
+        <div className="flex flex-col md:ml-[100px]">
+          <div className="mt-2 text-center md:text-left">
+            <TypeAnimation
+              sequence={[
+                "I'm Nishant",
+                1000,
+                "I'm Web Dev",
+                1000,
+                "I'm Frontend Dev",
+                1000,
+                "I'm Backend Dev",
+                1000,
+              ]}
+              className="text-orange-500 font-bold text-[30px] md:text-5xl"
+              wrapper="span"
+              speed={50}
+              style={{ display: "inline-block" }}
+              repeat={Infinity}
+            />
+            <div>
+              <button
+                type="button"
+                className="text-white bg-gradient-to-r mt-10 from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Hire me
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="my-5 md:max-w-xl">
-        <img className="rounded-lg max-h-full" src={photo} />
+
+      <div className="flex-1">
+        <img
+          className="md:pt-[]"
+          src={photo}
+          alt="Your Image"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
       </div>
     </section>
   );
