0c705abd29286cd444c0e9c0b3b249ce0443ba7e
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-05-07 19:05:06 +0545

    change

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,10 +6,13 @@ const Footer = () => {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
-          <NavLink to="/" className="hover:underline">
-            Nishant
+          <NavLink
+            to="https://www.nishantshiwakoti.com.np/"
+            className="hover:underline text-orange-400"
+          >
+            Nishant Siwakoti
           </NavLink>
-          . All Rights Reserved.
+          . | All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -13,41 +13,79 @@ import d from "../assets/images/4.png";
 import e from "../assets/images/5.png";
 import f from "../assets/images/6.png";
 import g from "../assets/images/7.png";
+import ma from "../assets/images/ma.png";
+import mb from "../assets/images/mb.png";
+import mc from "../assets/images/mc.png";
+import md from "../assets/images/md.png";
+import me from "../assets/images/me.png";
 
 const Slider = () => {
   return (
-    <div>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={2}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-      >
-        <SwiperSlide>
-          <img src={a} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={b} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={c} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={d} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={e} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={f} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={g} />
-        </SwiperSlide>
-      </Swiper>
-    </div>
+    <>
+      <div>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={2}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+        >
+          <SwiperSlide>
+            <img src={a} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={b} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={c} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={d} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={e} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={f} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={g} />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div>
+        <h1 className="text-2xl md:text-2xl text-orange-400 text-center mt-5 font-bold mb-5">
+          2. MovieMadHouse
+        </h1>
+      </div>
+      <div>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={2}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+        >
+          <SwiperSlide>
+            <img src={ma} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={mb} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={mc} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={md} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={me} />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
   );
 };
 
