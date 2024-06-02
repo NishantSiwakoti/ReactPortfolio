d4f3527b64806d01567de9dfc5d8bdd6533a20d5
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-06-02 13:25:21 +0545

    changes

--- src/Components/Div.jsx
+++ src/Components/Div.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 const getDateLabel = (providedDate, startTime) => {
@@ -45,7 +45,25 @@ const getDateLabel = (providedDate, startTime) => {
 };
 
 const Div = (props) => {
+  const [isMatchOver, setIsMatchOver] = useState(false);
   const dateLabel = getDateLabel(props.date, props.startTime);
+
+  useEffect(() => {
+    const startTime = new Date(props.date);
+    startTime.setHours(props.startTime); // Set start time of the match
+    const endTime = new Date(
+      startTime.getTime() + 8 * 60 * 60 * 1000 + 10 * 60 * 1000
+    ); // Calculate end time
+
+    const timer = setTimeout(() => {
+      // Check if current time is after end time
+      if (new Date() > endTime) {
+        setIsMatchOver(true); // Set match over flag to true
+      }
+    }, endTime - new Date());
+
+    return () => clearTimeout(timer); // Clean up the timer on component unmount
+  }, [props.date, props.startTime]);
 
   const isLive = dateLabel === "Live Now";
 
@@ -59,10 +77,14 @@ const Div = (props) => {
   // Apply color based on dateLabel
   if (dateLabel === "Live Now") {
     buttonStyle.backgroundColor = "#FF0000"; // Red
-  } else if (dateLabel === "Ended") {
+  } else if (dateLabel === "Ended" || isMatchOver) {
     buttonStyle.backgroundColor = "#808080"; // Gray
   } else {
     buttonStyle.backgroundColor = "#008000"; // Green
+  }
+
+  if (isMatchOver) {
+    return null; // Render nothing if match is over
   }
 
   return (
