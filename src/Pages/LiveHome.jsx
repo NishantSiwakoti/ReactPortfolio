7b7c7ef27c1153608ad527fb54dd91b83d206c9b
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-06-05 19:59:38 +0545

    changes

--- src/Pages/LiveHome.jsx
+++ src/Pages/LiveHome.jsx
@@ -20,7 +20,6 @@ import eng from "../assets/images/countries/eng.png";
 import afg from "../assets/images/countries/afg.png";
 import can from "../assets/images/countries/can.png";
 import nam from "../assets/images/countries/nam.png";
-import pn from "../assets/images/countries/pn.png";
 import sco from "../assets/images/countries/sco.png";
 import Div from "../Components/Div";
 
@@ -42,32 +41,20 @@ const LiveHome = ({ setProgress, title }) => {
     document.title = `${title}`;
   }, [title]);
 
-  const formatDate = (dateStr) => {
+  const formatDate = (dateStr, startTime) => {
     const matchDate = new Date(dateStr);
     const today = new Date();
     const currentTime = today.getHours() * 60 + today.getMinutes();
-    const matchTimeMorningStart = 6 * 60; // 6:00 AM in minutes
-    const matchTimeMorningEnd = 11 * 60; // 11:00 AM in minutes
-    const matchTimeEveningStart = 20 * 60 + 15; // 8:15 PM in minutes
-    const matchTimeEveningEnd = 24 * 60; // 12:00 AM midnight in minutes
+    const matchStartTime = startTime * 60; // convert start time to minutes
+    const matchEndTime = matchStartTime + 5 * 60; // 5 hours from start time
 
     if (matchDate.toDateString() === today.toDateString()) {
-      if (currentTime < matchTimeMorningStart) {
-        const countdownTime = matchTimeMorningStart - currentTime;
+      if (currentTime < matchStartTime) {
+        const countdownTime = matchStartTime - currentTime;
         return `Countdown: ${Math.floor(countdownTime / 60)}h ${
           countdownTime % 60
         }m`;
-      } else if (
-        currentTime >= matchTimeMorningStart &&
-        currentTime < matchTimeMorningEnd
-      ) {
-        return "Live";
-      } else if (currentTime < matchTimeEveningStart) {
-        return "Ended";
-      } else if (
-        currentTime >= matchTimeEveningStart &&
-        currentTime < matchTimeEveningEnd
-      ) {
+      } else if (currentTime >= matchStartTime && currentTime < matchEndTime) {
         return "Live";
       } else {
         return "Ended";
@@ -75,13 +62,8 @@ const LiveHome = ({ setProgress, title }) => {
     } else if (matchDate > today) {
       const diffDays = Math.ceil((matchDate - today) / (1000 * 60 * 60 * 24));
       if (diffDays === 1) {
-        if (currentTime < matchTimeMorningStart) {
-          const countdownTime = matchTimeMorningStart + (24 * 60 - currentTime);
-          return `Countdown: ${Math.floor(countdownTime / 60)}h ${
-            countdownTime % 60
-          }m`;
-        } else if (currentTime < matchTimeEveningStart) {
-          const countdownTime = matchTimeEveningStart + (24 * 60 - currentTime);
+        if (currentTime < matchStartTime) {
+          const countdownTime = matchStartTime + (24 * 60 - currentTime);
           return `Countdown: ${Math.floor(countdownTime / 60)}h ${
             countdownTime % 60
           }m`;
@@ -93,13 +75,126 @@ const LiveHome = ({ setProgress, title }) => {
     }
   };
 
-  const handleMatchStatus = (dateStr) => {
-    const status = formatDate(dateStr);
+  const handleMatchStatus = (dateStr, startTime) => {
+    const status = formatDate(dateStr, startTime);
     if (status === "Ended" && !endedMatches.includes(dateStr)) {
       setEndedMatches((prevEndedMatches) => [...prevEndedMatches, dateStr]);
     }
     return status;
   };
+
+  const matches = [
+    {
+      photo1: usa,
+      team1: "USA",
+      photo2: can,
+      team2: "Canada",
+      date: "2024/06/02",
+      startTime: 6,
+      time: "6:00 AM",
+      match: "Match 1",
+    },
+    {
+      photo1: wi,
+      team1: "West Indies",
+      photo2: png,
+      team2: "PNG",
+      date: "2024-06-02",
+      startTime: 19,
+      time: "7:00 PM",
+      match: "Match 2",
+    },
+    {
+      photo1: nam,
+      team1: "Namibia",
+      photo2: oman,
+      team2: "Oman",
+      date: "2024-06-03",
+      startTime: 5,
+      time: "5:00 AM",
+      match: "Match 3",
+    },
+    {
+      photo1: sri,
+      team1: "Sri Lanka",
+      photo2: sa,
+      team2: "South Africa",
+      date: "2024-06-03",
+      startTime: 19,
+      time: "7:00 PM",
+      match: "Match 4",
+    },
+    {
+      photo1: afg,
+      team1: "Afghanistan",
+      photo2: ug,
+      team2: "Uganda",
+      date: "2024-06-04",
+      startTime: 5,
+      time: "5:00 AM",
+      match: "Match 5",
+    },
+    {
+      photo1: eng,
+      team1: "England",
+      photo2: sco,
+      team2: "Scotland",
+      date: "2024-06-04",
+      startTime: 19,
+      time: "8:15 PM",
+      match: "Match 6",
+    },
+    {
+      photo1: nep,
+      team1: "Nepal",
+      photo2: ned,
+      team2: "Netherland",
+      date: "2024-06-04",
+      startTime: 20,
+      time: "9:15 PM",
+      match: "Match 7",
+    },
+    {
+      photo1: ind,
+      team1: "India",
+      photo2: ire,
+      team2: "Ireland",
+      date: "2024-06-05",
+      startTime: 19,
+      time: "8:15 PM",
+      match: "Match 8",
+    },
+    {
+      photo1: png,
+      team1: "Papua New Guinea",
+      photo2: ug,
+      team2: "Uganda",
+      date: "2024-06-06",
+      startTime: 5,
+      time: "5:15 AM",
+      match: "Match 9",
+    },
+    {
+      photo1: aus,
+      team1: "Australia",
+      photo2: oman,
+      team2: "Oman",
+      date: "2024-06-06",
+      startTime: 6,
+      time: "6:15 AM",
+      match: "Match 10",
+    },
+    {
+      photo1: usa,
+      team1: "USA",
+      photo2: pak,
+      team2: "Pakistan",
+      date: "2024-06-06",
+      startTime: 20,
+      time: "9:15 PM",
+      match: "Match 11",
+    },
+  ];
 
   return (
     <section className="min-h-screen">
@@ -107,117 +202,24 @@ const LiveHome = ({ setProgress, title }) => {
         World Cup Matches
       </h2>
 
-      <Div
-        photo1={usa}
-        team1="USA"
-        photo2={can}
-        team2="Canada"
-        date="2024/06/02"
-        startTime={6} // Live stream starts at 6 am
-        time="6:00 AM"
-        match="Match 1"
-      />
-      <Div
-        photo1={wi}
-        team1="West Indies"
-        photo2={png}
-        team2="PNG"
-        date="2024-06-02"
-        startTime={19} // Live stream starts at 6 am
-        time="7:00 PM"
-        match="Match 2"
-      />
-      <Div
-        photo1={nam}
-        team1="Namibia"
-        photo2={oman}
-        team2="Oman"
-        date="2024-06-03"
-        startTime={5} // Live stream starts at 6 am
-        time="5:00 AM"
-        match="Match 3 "
-      />
+      {matches.map((match, index) => {
+        const status = handleMatchStatus(match.date, match.startTime);
+        if (status === "Ended") return null;
+        return (
+          <Div
+            key={index}
+            photo1={match.photo1}
+            team1={match.team1}
+            photo2={match.photo2}
+            team2={match.team2}
+            date={match.date}
+            startTime={match.startTime}
+            time={match.time}
+            match={match.match}
+          />
+        );
+      })}
 
-      <Div
-        photo1={sri}
-        team1="Sri Lanka"
-        photo2={sa}
-        team2="South Africa"
-        date="2024-06-03"
-        startTime={19} // Live stream starts at 6 am
-        time="7:00 PM"
-        match="Match 4"
-      />
-      <Div
-        photo1={afg}
-        team1="Afghanistan"
-        photo2={ug}
-        team2="Uganda"
-        date="2024-06-04"
-        startTime={5} // Live stream starts at 6 am
-        time="5:00 AM"
-        match="Match 5"
-      />
-      <Div
-        photo1={eng}
-        team1="England"
-        photo2={sco}
-        team2="Scotland"
-        date="2024-06-04"
-        startTime={19} // Live stream starts at 6 am
-        time="8:15 PM"
-        match="Match 6"
-      />
-      <Div
-        photo1={nep}
-        team1="Nepal"
-        photo2={ned}
-        team2="Netherland"
-        date="2024-06-04"
-        startTime={20} // Live stream starts at 6 am
-        time="9:15 PM"
-        match="Match 7"
-      />
-      <Div
-        photo1={ind}
-        team1="India"
-        photo2={ire}
-        team2="Ireland"
-        date="2024-06-05"
-        startTime={19} // Live stream starts at 6 am
-        time="8:15 PM"
-        match="Match 8"
-      />
-      <Div
-        photo1={png}
-        team1="Papua New Guinea"
-        photo2={ug}
-        team2="Uganda"
-        date="2024-06-06"
-        startTime={5} // Live stream starts at 6 am
-        time="5:15 AM"
-        match="Match 9"
-      />
-      <Div
-        photo1={aus}
-        team1="Australia"
-        photo2={oman}
-        team2="Oman"
-        date="2024-06-06"
-        startTime={6} // Live stream starts at 6 am
-        time="6:15 AM"
-        match="Match 10"
-      />
-      <Div
-        photo1={usa}
-        team1="USA"
-        photo2={pak}
-        team2="Pakistan"
-        date="2024-06-06"
-        startTime={20} // Live stream starts at 6 am
-        time="9:15 PM"
-        match="Match 11"
-      />
       <div className="flex justify-center">
         <div className="w-full max-w-3xl p-4 m-5">
           <p className="text-black dark:text-white font-bold text-base text-center p-2">
