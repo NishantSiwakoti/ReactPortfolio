2107be5ad8b6e27910bb6853efdee58cb340c7f3
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-06-06 21:44:35 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import { AllRoutes } from "./Routes/AllRoutes";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <AllRoutes />
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
