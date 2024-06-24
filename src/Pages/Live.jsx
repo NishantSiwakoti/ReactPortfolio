e8dee8e2a27ec4c81b14ac6098e7ba4392490f44
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-06-24 20:51:53 +0545

    chages

--- src/Pages/Live.jsx
+++ src/Pages/Live.jsx
@@ -10,13 +10,9 @@ const Live = ({ setProgress, title }) => {
       language: "English Stream",
       url: "https://streambtw.com/iframe/e1.php",
     },
-    // {
-    //   language: "Hindi Stream",
-    //   url: "https://emdftinya.tinyuri.org/embed/hindi.php",
-    // },
     {
-      language: "Englishh Stream",
-      url: "https://dlhd.so/embed/stream-346.php",
+      language: "Hindi Stream",
+      url: "https://mm5-97.tinyuri.org/live/m4-720p.m3u8",
     },
   ]);
   const [currentStreamUrl, setCurrentStreamUrl] = useState(
