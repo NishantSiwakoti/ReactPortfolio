ebefb04691ca734d95c999c387f5dcb0d1e21cfa
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-07-02 11:51:49 +0545

    changes

--- src/Pages/Football.jsx
+++ src/Pages/Football.jsx
@@ -1,24 +1,12 @@
 import { useEffect } from "react";
 import Div from "../Components/Div";
 import Germany from "../assets/images/football/ger.png";
-import al from "../assets/images/football/al.png";
 import Austria from "../assets/images/football/aus.png";
-import Belgium from "../assets/images/football/bel.png";
-import cr from "../assets/images/football/cr.png";
-import cz from "../assets/images/football/cz.png";
-import Denmark from "../assets/images/football/den.png";
 import England from "../assets/images/football/eng.png";
 import France from "../assets/images/football/fra.png";
-import Georgia from "../assets/images/football/geo.png";
-import hun from "../assets/images/football/hun.png";
-import Italy from "../assets/images/football/ita.png";
 import Netherlands from "../assets/images/football/ned.png";
-import pol from "../assets/images/football/pol.png";
 import Portugal from "../assets/images/football/por.png";
 import Romania from "../assets/images/football/rom.png";
-import ser from "../assets/images/football/ser.png";
-import Slovakia from "../assets/images/football/sloa.png";
-import Slovenia from "../assets/images/football/sloe.png";
 import Spain from "../assets/images/football/spa.png";
 import Switzerland from "../assets/images/football/swi.png";
 import Turkiye from "../assets/images/football/tur.png";
@@ -36,83 +24,24 @@ const Football = ({ setProgress, title }) => {
     document.title = `${title}`;
   }, [title]);
 
+  const countryColors = {
+    Romania: "#055c9d", // Blue from the Romanian flag
+    Netherlands: "#21468B", // Blue from the Dutch flag
+    Austria: "#ED2939", // Red from the Austrian flag
+    Turkiye: "#ff3535", // Red from the Turkish flag
+    Spain: "#C60B1E", // Red from the Spanish flag
+    Germany: "#333333", // Black from the German flag
+    Portugal: "#006600", // Green from the Portuguese flag
+    France: "#0055A4", // Blue from the French flag
+    England: "#00247D", // Blue from the English flag (UK Union Jack)
+    Switzerland: "#D52B1E", // Red from the Swiss flag
+  };
+
   const matches = [
     {
       startTime: "21:01",
       time: "9:45 PM",
-      endTime: "11:45",
-      startDate: "June 29, 2024",
-      endDate: "June 29, 2024",
-      photo1: Switzerland,
-      team1: "Switzerland",
-      photo2: Italy,
-      team2: "Italy",
-      match: "Round of 16",
-    },
-    {
-      startTime: "00:01",
-      time: "12:45 AM",
-      endTime: "2:45",
-      startDate: "June 30, 2024",
-      endDate: "June 30, 2024",
-      photo1: Germany,
-      team1: "Germany",
-      photo2: Denmark,
-      team2: "Denmark",
-      match: "Round of 16",
-    },
-    {
-      startTime: "21:01",
-      time: "9:45 PM",
-      endTime: "11:45",
-      startDate: "June 30, 2024",
-      endDate: "June 30, 2024",
-      photo1: England,
-      team1: "England",
-      photo2: Slovakia,
-      team2: "Slovakia",
-      match: "Round of 16",
-    },
-    {
-      startTime: "00:01",
-      time: "12:45 PM",
-      endTime: "2:45",
-      startDate: "July 1, 2024",
-      endDate: "July 1, 2024",
-      photo1: Spain,
-      team1: "Spain",
-      photo2: Georgia,
-      team2: "Georgia",
-      match: "Round of 16",
-    },
-    {
-      startTime: "21:01",
-      time: "9:45 PM",
-      endTime: "11:45",
-      startDate: "July 1, 2024",
-      endDate: "July 1, 2024",
-      photo1: France,
-      team1: "France",
-      photo2: Belgium,
-      team2: "Belgium",
-      match: "Round of 16",
-    },
-    {
-      startTime: "00:01",
-      time: "12:45 AM",
-      endTime: "2:45",
-      startDate: "July 2, 2024",
-      endDate: "July 2, 2024",
-      photo1: Portugal,
-      team1: "Portugal",
-      photo2: Slovenia,
-      team2: "Slovenia",
-      match: "Round of 16",
-    },
-    {
-      startTime: "21:01",
-      time: "9:45 PM",
-      endTime: "11:45",
+      endTime: "23:45",
       startDate: "July 2, 2024",
       endDate: "July 2, 2024",
       photo1: Romania,
@@ -120,6 +49,8 @@ const Football = ({ setProgress, title }) => {
       photo2: Netherlands,
       team2: "Netherlands",
       match: "Round of 16",
+      leftDivColor: countryColors.Romania,
+      rightDivColor: countryColors.Netherlands,
     },
     {
       startTime: "00:01",
@@ -132,6 +63,8 @@ const Football = ({ setProgress, title }) => {
       photo2: Turkiye,
       team2: "Turkiye",
       match: "Round of 16",
+      leftDivColor: countryColors.Austria,
+      rightDivColor: countryColors.Turkiye,
     },
     {
       startTime: "21:01",
@@ -144,14 +77,44 @@ const Football = ({ setProgress, title }) => {
       photo2: Germany,
       team2: "Germany",
       match: "Quater-Final",
+      leftDivColor: countryColors.Spain,
+      rightDivColor: countryColors.Germany,
+    },
+    {
+      startTime: "00:01",
+      time: "12:45 AM",
+      endTime: "2:45",
+      startDate: "July 6, 2024",
+      endDate: "July 6, 2024",
+      photo1: Portugal,
+      team1: "Portugal",
+      photo2: France,
+      team2: "France",
+      match: "Quater-Final",
+      leftDivColor: countryColors.Portugal,
+      rightDivColor: countryColors.France,
+    },
+    {
+      startTime: "21:01",
+      time: "9:45 PM",
+      endTime: "23:45",
+      startDate: "July 6, 2024",
+      endDate: "July 6, 2024",
+      photo1: England,
+      team1: "England",
+      photo2: Switzerland,
+      team2: "Switzerland",
+      match: "Quater-Final",
+      leftDivColor: countryColors.England,
+      rightDivColor: countryColors.Switzerland,
     },
   ];
 
   return (
     <section>
       <div className="min-h-screen">
-        <div className="flex justify-center md:justify-end mt-5 items-center ">
-          <Link to="/livematches" className="">
+        <div className="flex justify-center md:justify-end mt-5 items-center">
+          <Link to="/livematches">
             <div className="px-3 py-2 mr-2 bg-gradient-to-r from-green-500 via-green-600 to-green-700 text-white rounded-lg">
               Cricket Matches
             </div>
@@ -175,17 +138,19 @@ const Football = ({ setProgress, title }) => {
             match={match.match}
             team1={match.team1}
             team2={match.team2}
+            leftDivColor={match.leftDivColor}
+            rightDivColor={match.rightDivColor}
           />
         ))}
       </div>
       <div className="flex justify-center">
         <div className="w-full max-w-3xl p-4 m-5">
           <p className="text-black dark:text-white font-bold text-base text-center p-2">
-            Digital Millennium Copyright Act(DMCA)
+            Digital Millennium Copyright Act (DMCA)
           </p>
           <p className="text-black dark:text-white text-sm font-normal italic">
             This site only contains links and embeds to TV channels from 3rd
-            party sites Which are freely available on all Internet. We are not
+            party sites which are freely available on all Internet. We are not
             affiliated in any way with the broadcasted channels nor responsible
             for their content. All content is copyright of their respective
             owners.
