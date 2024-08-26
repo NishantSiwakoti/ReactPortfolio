4134fb6b40c55b96d83a12036a96a3f48571a2f5
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-08-26 17:35:08 +0545

    changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -38,41 +38,16 @@ const Header = () => {
             </span>
           </NavLink>
           <div className="flex items-center md:order-2">
-            <button
-              onClick={() => setDarkMode(!darkMode)}
-              data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip"
-              type="button"
-              data-toggle-dark="light"
-              className="md:hidden flex items-center p-2 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-[#29c964] focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-dark-900 dark:hover:bg-gray-700"
-            >
-              {darkMode ? (
-                <svg
-                  aria-hidden="true"
-                  data-toggle-icon="sun"
-                  className="w-4 h-4"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              ) : (
-                <svg
-                  aria-hidden="true"
-                  data-toggle-icon="moon"
-                  className="w-4 h-4 hover:text-[#29c964]"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                </svg>
-              )}
-            </button>
+            {/* Dark mode switch */}
+            <label className="switch">
+              <input
+                type="checkbox"
+                checked={darkMode}
+                onChange={() => setDarkMode(!darkMode)}
+              />
+              <span className="slider"></span>
+            </label>
+
             <button
               onClick={() => setHidden(!hidden)}
               data-collapse-toggle="navbar-default"
@@ -117,14 +92,6 @@ const Header = () => {
                   Home
                 </NavLink>
               </li>
-              {/* <li>
-                <NavLink
-                  to="/live"
-                  className={isLiveActive ? activeClass : inActiveClass}
-                >
-                  Live
-                </NavLink>
-              </li> */}
               <li>
                 <NavLink
                   to="/about"
@@ -135,16 +102,6 @@ const Header = () => {
                   About
                 </NavLink>
               </li>
-              {/* <li>
-                <NavLink
-                  to="/blogs"
-                  className={({ isActive }) =>
-                    isActive ? activeClass : inActiveClass
-                  }
-                >
-                  Blogs
-                </NavLink>
-              </li> */}
               <li>
                 <NavLink
                   to="/skills"
@@ -174,43 +131,6 @@ const Header = () => {
                 >
                   Contact
                 </NavLink>
-              </li>
-              <li className="hidden md:block">
-                <button
-                  onClick={() => setDarkMode(!darkMode)}
-                  data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip"
-                  type="button"
-                  data-toggle-dark="light"
-                  className="flex items-center p-2 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-[#29c964] focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-dark-900 dark:hover:bg-gray-700"
-                >
-                  {darkMode ? (
-                    <svg
-                      aria-hidden="true"
-                      data-toggle-icon="sun"
-                      className="w-4 h-4"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  ) : (
-                    <svg
-                      aria-hidden="true"
-                      data-toggle-icon="moon"
-                      className="w-4 h-4 hover:text-[#29c964]"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                    </svg>
-                  )}
-                </button>
               </li>
             </ul>
           </div>
